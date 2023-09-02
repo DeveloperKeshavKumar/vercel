ad8597e5930f2034695c930c69cabb7cce7d30fa
KeshavKumar <keshav0kumar@hotmail.com>
2023-09-02 15:01:07 +0530

    ejs added

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 // Initialize Express
 const app = express();
+app.use(express.static('public'))
+app.set('views', './views')
+app.set('view engine', 'ejs')
 
 // Create GET request
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.render('index',{msg:"Working Fine"})
 });
 
 // Initialize server
